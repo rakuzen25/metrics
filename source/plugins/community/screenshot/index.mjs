abcf669ab058aca18eff2c5ff82368afbf7db57c
Frank <25704248+rakuzen25@users.noreply.github.com>
2026-06-19 13:17:16 -0500

    feat(screenshot): implement lazy load

--- source/plugins/community/screenshot/index.mjs
+++ source/plugins/community/screenshot/index.mjs
@@ -7,7 +7,7 @@ export default async function({login, q, imports, data, account}, {enabled = fal
       return null
 
     //Load inputs
-    let {url, selector, title, background, viewport, wait, mode} = imports.metadata.plugins.screenshot.inputs({data, account, q})
+    let {url, selector, title, background, viewport, lazyload, wait, mode} = imports.metadata.plugins.screenshot.inputs({data, account, q})
     if (!url)
       throw {error: {message: "URL is not set"}}
 
@@ -19,6 +19,40 @@ export default async function({login, q, imports, data, account}, {enabled = fal
     await page.setViewport(viewport)
     console.debug(`metrics/compute/${login}/plugins > screenshot > loading ${url}`)
     await page.goto(url, {waitUntil: ["domcontentloaded", "networkidle2"]})
+
+    await page.waitForSelector(selector)
+    if (lazyload) {
+      await page.evaluate(async sel => {
+        const element = document.querySelector(sel)
+        if (!element)
+          return
+
+        //Progressive scroll
+        await new Promise(resolve => {
+          const timer = setInterval(() => {
+            const rect = element.getBoundingClientRect()
+            if (rect.bottom <= window.innerHeight || window.scrollY + window.innerHeight >= document.body.scrollHeight) {
+              clearInterval(timer)
+              resolve()
+            }
+            else {
+              window.scrollBy(0, 300)
+            }
+          }, 100)
+        })
+
+        await Promise.all([...element.querySelectorAll("img")].map(img =>
+          new Promise(solve => {
+            if (img.complete)
+              solve()
+            else {
+              img.addEventListener("load", solve)
+              img.addEventListener("error", solve)
+            }
+          })
+        ))
+      }, selector)
+    }
     if (wait)
       await new Promise(solve => setTimeout(solve, wait))
 
@@ -26,12 +60,16 @@ export default async function({login, q, imports, data, account}, {enabled = fal
     let content = null
     let image = null
     const metadata = {height: null, width: null}
-    await page.waitForSelector(selector)
     switch (mode) {
       case "image": {
         const clip = await page.evaluate(selector => {
           const {x, y, width, height} = document.querySelector(selector).getBoundingClientRect()
-          return {x, y, width, height}
+          return {
+            x: x + window.scrollX,
+            y: y + window.scrollY,
+            width,
+            height,
+          }
         }, selector)
         console.debug(`metrics/compute/${login}/plugins > screenshot > coordinates ${JSON.stringify(clip)}`)
         const [buffer] = await imports.record({page, ...clip, frames: 1, background})
